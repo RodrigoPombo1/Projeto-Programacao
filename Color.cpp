28abccbabec64ac8c11b549551381b773a5ffe40
Rodrigo Pombo <rodrigogasparpombo@gmail.com>
2023-04-30 00:10:43 +0100

    implementação do color.cpp

--- Color.cpp
+++ Color.cpp
@@ -1,35 +1,33 @@
 #include "Color.hpp"
 
 namespace prog {
-    Color::Color() {
-
-    }
+    Color::Color() : Color::Color(0, 0, 0) {}
     Color::Color(const Color& other) {
-
+        this->_red = other._red;
+        this->_green = other._green;
+        this->_blue = other._blue;
     }
     Color::Color(rgb_value red, rgb_value green, rgb_value blue) {
-
+        this->_red = red;
+        this->_green = green;
+        this->_blue = blue;
     }
     rgb_value Color::red() const {
-        return 0;
+        return _red;
     }
     rgb_value Color::green() const {
-        return 0;
+        return _green;
     }
     rgb_value Color::blue() const {
-        return 0;
+        return _blue;
     }
-
-    // TODO: remove this DUMMY variable once you have appropriate fields for representing colors.
-    rgb_value DUMMY_rgb_value = 0; 
-
     rgb_value& Color::red()  {
-        return DUMMY_rgb_value;
+        return _red;
     }
     rgb_value& Color::green()  {
-      return DUMMY_rgb_value;
+      return _green;
     }
     rgb_value& Color::blue()  {
-      return DUMMY_rgb_value;
+      return _blue;
     }
 }
